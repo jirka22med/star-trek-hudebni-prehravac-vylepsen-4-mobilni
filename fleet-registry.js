43b22139f93884cda6836eec54dd6b35cc94b569
jirka22med <153131598+jirka22med@users.noreply.github.com>
2026-01-01 02:10:40 +0100

    Update fleet-registry.js

--- fleet-registry.js
+++ fleet-registry.js
@@ -1,6 +1,6 @@
 // fleet-registry.js - VELITELSKÝ MOST (Jediný soubor, který upravuješ)
 const FLEET_CONFIG = {
-    version: "599.999.250.1", // ← Zvýšil jsem o 1 (nový modul přidán)
+    version: "599.999.250.2", // ← Zvýšil jsem o 1 (nový modul přidán)
     codename: "Prometheus-Class",
     
     // SEZNAM VŠECH MODULŮ (Tady spravuješ odkazy)
